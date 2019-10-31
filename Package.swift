21da8e10bac032e0add78d3e6608a67cee39fe81
Arkadi Yoskovitz <a4s.development@gmail.com>
2019-10-31 12:30:39 +0200

    Add support for iOS 9

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "AFSAppDispatcherKit",
-    platforms: [ .iOS(.v10)],//, .macOS(.v10_15), .watchOS(.v6), .tvOS(.v10)],
+    platforms: [ .iOS(.v9)],//, .macOS(.v10_15), .watchOS(.v6), .tvOS(.v10)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
